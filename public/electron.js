fde13032312956d6791623c26128ba0f5eae8e9e
Nicolas Leveque <nicoleveque@outlook.com>
2021-08-14 15:09:05 +0200

    first commit

---
+++ public/electron.js
@@ -0,0 +1,58 @@
+const path = require("path")
+
+const { app, BrowserWindow } = require("electron")
+const isDev = require("electron-is-dev")
+
+let installExtension, REACT_DEVELOPER_TOOLS; 
+if (isDev) {
+    const devTools = require("electron-devtools-installer")
+    installExtension = devTools.default
+    REACT_DEVELOPER_TOOLS = devTools.REACT_DEVELOPER_TOOLS;
+}
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadURL(
+        isDev
+            ? 'http://localhost:3000'
+            : `file://${path.joim(__dirname, "../build/index.html")}`
+    )
+
+    if (isDev) {
+        win.webContents.openDevTools({ mode: "detach" })
+    }
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    if (isDev) {
+        installExtension(REACT_DEVELOPER_TOOLS)
+            .then(name => console.log(`Added Extension: ${name}`))
+            .catch(error => console.log(`An error occured: , ${error}`))
+    }
+})
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit()
+    }
+})
+
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
+
+
+if (require("electron-squirrel-startup")) {
+    app.quit()
+}
